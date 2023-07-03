f8350abb7e00bc40fba85e8f09974b9f18cd7b3f
yanyue404 <xiaoyueyue165@gmail.com>
2023-07-03 18:32:36 +0800

    chore: 深入响应式例子

--- examples/simple/app.js
+++ examples/simple/app.js
@@ -1,17 +1,28 @@
 /* global Vue */
 
-new Vue({
+// watcher(name: bbb, id: 1)
+// watcher(name: count, id: 2)
+// 渲染 watcher name: render id: 3
+
+// new Wacther(()=> updateComponent(render()) )
+
+// TODO: 每一个dep 收集的watcher是什么样子，每一个桶的变化，如果有组件呢？
+// 常见问题：computed watcher 的缓存机制
+
+const vm = new Vue({
   el: "#app",
   data: {
-    key: "Vue！",
-    parentMessage: "Parent",
-    items: [{ message: "Foo" }, { message: "Bar" }],
-    count: 0,
+    key: "Vue！", // []
+    parentMessage: "Parent", // [
+    items: [{ message: "Foo" }, { message: "Bar" }], //  []
+    count: 0, //  []
+    aaa: 123, // []
   },
+  // bbb get set  []
   computed: {
-    text() {
-      return "hello, " + this.key;
-    },
+    bbb() {
+      return this.count + this.key
+    }
   },
   methods: {
     handleClick() {
@@ -24,3 +35,5 @@ new Vue({
     console.dir(document.getElementById("app"));
   },
 });
+
+vm.$watch('count', ()=> {})
